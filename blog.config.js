95cc4fd1b1ce4d4f2e53eb958cca65c44db16a18
srn <18543415@qq.com>
2026-04-27 10:45:32 +0800

    初始化

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: srn
  * @Date: 2026-04-27 09:09:18
- * @LastEditTime: 2026-04-27 10:38:37
+ * @LastEditTime: 2026-04-27 10:42:53
  * @FilePath: /NotionNextRui/blog.config.js
  */
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
@@ -22,7 +22,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'RUIRUI', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的程序员👨🏻‍💻', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一名分享干货的程序员👨🏻‍💻', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://skrlx.ccwu.cc/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'RUIRUI, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    // { title: '欢迎来到我的博客', url: 'https://skrlx.ccwu.cc/' },
+    { title: '欢迎来到我的博客', url: 'https://skrlx.ccwu.cc/' }
     // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享干货',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'skrlx.ccwu.cc',
   HEO_HERO_TITLE_4: '新版上线',
